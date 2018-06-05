b09cce41dc8b4dde246121e3ab50be3c9f8765eb
moxDroid <pritam.world@gmail.com>
2018-06-05 19:50:10 -0400

    Advance code added

---
+++ src/collegesysteminheritance/Global.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collegesysteminheritance;
+
+/**
+ *
+ * @author moxdroid
+ */
+public class Global 
+{
+    {
+        System.out.println("Hello LINE 1");
+    }
+    int a = 9099;
+    int b = 50000;
+    {
+        int a = 400;
+        System.out.println(this.a);
+         System.out.println(a);
+          System.out.println(b);
+    }
+    
+    static String NAME;
+    static
+    {
+        int x = 10;
+        int y = 200;
+        System.out.println(x);
+        NAME = "Pritesh Patel";
+    }
+}
